d34fc76b243d1e65a2e9809f9ad507c523c79847
Maginele Larins <100315803ml@gmail.com>
2022-05-25 12:30:52 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let joSnowAttack = 25;
+let jamieLannisterAttack = 35;
+if (joSnowAttack > jamieLannisterAttack) {
+    console.log('jon Snow has better attack than jamie Lannister'); 
+}
+else { 
+    console.log('jamie Lannister has better attck than Jon Snow')
+}
